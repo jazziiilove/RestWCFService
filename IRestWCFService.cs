8ea859cc70e55b6719b740aebf74cc2d8daccdcd
baran topal <barantopal@barantopal.com>
2017-07-13 16:39:09 +0200

    initial commit

---
+++ IRestWCFService.cs
@@ -0,0 +1,72 @@
+﻿/* 
+ * Programmer: Baran Topal                       *
+ * Solution name: RestWCFService                 * 
+ * Project name: RestWCFService                  *
+ * File name: IRestWCFService.cs                 *
+ *                                               *      
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *	                                                                                         *
+ *  LICENSE: This source file is subject to have the protection of GNU General               *
+ *	Public License. You can distribute the code freely but storing this license information. *
+ *	Contact Baran Topal if you have any questions. barantopal@barantopal.com                 *
+ *	                                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+// To call the GET method, run these in your browser:
+// http://localhost:52954/RestWCFServiceImpl.svc/json/345 and http://localhost:52954/RestWCFServiceImpl.svc/xml/123
+// localhost:port will be given by VS when you run it from VS.
+
+using System.IO;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace RestWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IRestWCFService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+
+        string XMLData(string id);
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+
+        string JSONData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Xml, UriTemplate = "DoSomeWork/create")]
+
+        string DoSomeWork(Stream data);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ RestWCFServiceImpl.svc.cs
@@ -0,0 +1,72 @@
+﻿/* 
+ * Programmer: Baran Topal                       *
+ * Solution name: RestWCFService                 * 
+ * Project name: RestWCFService                  *
+ * File name: RestWCFService.svc.cs              *
+ *                                               *      
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *	                                                                                         *
+ *  LICENSE: This source file is subject to have the protection of GNU General               *
+ *	Public License. You can distribute the code freely but storing this license information. *
+ *	Contact Baran Topal if you have any questions. barantopal@barantopal.com                 *
+ *	                                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace RestWCFService
+{
+
+    public class RestWCFServiceImpl : IRestWCFService
+    {
+        public string XMLData(string id)
+        {
+            return "You request product " + id;
+        }
+
+        public string JSONData(string id)
+        {
+            return "You request product " + id;
+        }
+
+
+        public string DoSomeWork(Stream data)
+        {
+
+            StreamReader reader = new StreamReader(data, Encoding.UTF8);
+
+            string xmlString = reader.ReadToEnd();
+
+            var xmlReader = XmlReader.Create(new StringReader(xmlString));
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlString);
+            var test = string.Empty;
+
+
+            using (var stringWriter = new StringWriter())
+            using (var xmlTextWriter = XmlWriter.Create(stringWriter))
+            {
+
+                xmlDoc.WriteTo(xmlTextWriter);
+                var node = xmlDoc.SelectSingleNode("//root/someNode");
+                if (node != null)
+                {
+                    node.InnerText = "baran";
+                    test = node.InnerText;
+                }
+                xmlTextWriter.Flush();
+
+                // test = stringWriter.GetStringBuilder().ToString();                               
+            }
+
+            Console.WriteLine("In");
+            return test;
+        }
+    }
+}
